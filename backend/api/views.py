482d2168bc71dc08e70ff2262e15a281ee591197
gradrrs <artemlal1998@gmail.com>
2026-05-16 04:01:12 +0300

    создание сериализоватора, маршрутизация и вьюхи

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+class EventListCreateView(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
