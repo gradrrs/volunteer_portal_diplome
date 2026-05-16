6f01d8085580867197a8ac6e7487ffbd8ba7e21d
gradrrs <artemlal1998@gmail.com>
2026-05-16 20:41:46 +0300

    добавление моделей в админку

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Event, Application, Post, Comment, Like, Rating, Notification
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Application)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Rating)
+admin.site.register(Notification)
