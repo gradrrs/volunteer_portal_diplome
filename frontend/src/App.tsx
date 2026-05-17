a9edb92a9bbe402f774b55e603fb6ec08c40e95b
gradrrs <artemlal1998@gmail.com>
2026-05-17 09:20:32 +0300

    инициализация фронтнда и настройка tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-center text-2xl text-blue-600">проверка</div>
+  )
+}
+
+export default App
