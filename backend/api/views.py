67aac9474f70ebe8f72b4b476d26b593657a9a0d
gradrrs <artemlal1998@gmail.com>
2026-06-14 19:25:33 +0300

    баг с админкой

--- backend/api/views.py
+++ backend/api/views.py
@@ -26,7 +26,7 @@ class EventListCreateView(generics.ListCreateAPIView):
             queryset = queryset.filter(title__icontains=search)
         if date:
             queryset = queryset.filter(date__date=date)
-        return queryset
+        return Application.objects.all()
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
